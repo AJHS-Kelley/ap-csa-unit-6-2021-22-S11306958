c5ce4c6393ec62188ca8e05a910fda5ba1d84e16
Randy Kaneer <s11306958@students.duvalschools.org>
2022-01-24 08:48:42 -0500

    Arrays have been added

---
+++ .github/JacksonStanton00.java
@@ -0,0 +1,20 @@
+// APCSA, Randy Kaneer, Array Practice, v0.0, 01/24/2022
+
+public class JacksonStanton00 {
+    public static void main(String[] args) {
+        String[] carArr = {"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford f150 Lightning", "Buick Skylark"};
+        int[] myGrades = {100, 25, 15, -3, 0, 75, 35};
+        double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 3.14}
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
