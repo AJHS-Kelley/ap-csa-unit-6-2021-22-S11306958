e213f4aecce714d1a541c4d013e0b6d746e510ae
Randy Kaneer <s11306958@students.duvalschools.org>
2022-01-24 09:06:48 -0500

    Update JacksonStanton00.java

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -1,10 +1,21 @@
-// APCSA, Randy Kaneer, Array Practice, v0.5, 01/24/2022
+// APCSA, Randy Kaneer, Array Practice, v1.0, 01/24/2022
 
 public class JacksonStanton00 {
     public static void main(String[] args) {
         String[] carArr = {"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford f150 Lightning", "Buick Skylark"};
         int[] myGrades = {100, 25, 15, -3, 0, 75, 35};
         double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
+
+        for (current_element = 0; current_element < carArr.length(); current_element++) {
+            //Code to execute
+            //Statement1 is executed once before the loop
+            //Statement2 is the conditional for the loop
+            //Statement3 is executed each time the loop runs
+
+        }
+
+
+
     }
 
 
